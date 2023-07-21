d22d903d456fc90ce13c88956742787db70feab7
Code Monkey King <freesoft.for.people@gmail.com>
2023-07-21 15:00:30 +0300

    made expression being sent as an arg

--- src/basic.py
+++ src/basic.py
@@ -1,80 +1,64 @@
-
-#src = [i for i in '-(-((((7 + 2) * 5) / (10 - 5)) + ((6 * 4) - (9 / 3))) - ((((8 + 2) - 1) * (4 + 6)) / (9 - 3)))'.replace(' ', '')]
-src = [i for i in '-(-((((7 + 2) * 5) / (numOne - 5)) + ((numTwo * 4) - (9 / 3))) - ((((8 + 2) - 1) * (4 + numTwo)) / (9 - 3)))'.replace(' ', '')]
-#src = [i for i in '((10-5)+20/2)*4-(23+17)']
-#src = [i for i in '((12+5)*(13-6))+10']  # works
-#src = [i for i in '((12+5)*-(-13-6))+10']  # works
-#src = [i for i in '2*10*3*5/3/20*10']  # works
-#src = [i for i in '12+34-7+117-24']    # works
-#src = [i for i in '2*10*3*5/3/20*10+10+34-7+117-24']  # works
-#src = [i for i in '23*(17-5)-42/7+(342-456)*5']  # works
-#src = [i for i in '(1+2+3)*(2+4)-6']  # works
-
-#src = [i for i in '2+']  # works
-
 buffer = []
 variables = {
   'numOne': 10,
   'numTwo': 6
 }; token = ''
 
-def expr():
-  a = term()
+def expression(expr):
+  a = term(expr)
   while True:
-    if token == '+': scan(); b = term(); a = a + b
-    elif token == '-': scan(); b = term(); a = a - b
-    else: return a  # check for errors if NULL
+    if token == '+': scan(expr); b = term(expr); a = a + b
+    elif token == '-': scan(expr); b = term(expr); a = a - b
+    else: return a
 
-def term():
-  a = factor()
+def term(expr):
+  a = factor(expr)
   while True:
-    scan()
-    if token == '*': scan(); b = factor(); a = a * b
-    elif token == '/': scan(); b = factor(); a = int(a / b)
-    else: return a  # check for errors if NULL
+    scan(expr)
+    if token == '*': scan(expr); b = factor(expr); a = a * b
+    elif token == '/': scan(expr); b = factor(expr); a = int(a / b)
+    else: return a
 
-def factor():
+def factor(expr):
   if type(token) == int: return token
   if token == '(':
-    scan(); a = expr()
+    scan(expr); a = expression(expr)
     if a == None: return None
     if token == ')': return a
     else: return None
-  elif token == '-': scan(); return -factor()
+  elif token == '-': scan(expr); return -factor(expr)
   else: return None
 
-def number():
+def number(expr):
   num = 0; tok = ''
-  while len(src) and src[0].isdigit():
-    tok += src[0]; del src[0]; num = int(tok)
+  while len(expr) and expr[0].isdigit():
+    tok += expr[0]; del expr[0]; num = int(tok)
   return num
 
-def variable():
+def variable(expr):
   name = ''
-  while len(src) and src[0].isalpha():
-    name += src[0]; del src[0]
+  while len(expr) and expr[0].isalpha():
+    name += expr[0]; del expr[0]
   if name not in variables: print('Variable "' + name + '" is not defined!')
   return variables[name]
 
-def operator():
-  op = src[0]
-  del src[0]
+def operator(expr):
+  op = expr[0]
+  del expr[0]
   return op
 
-def scan():
+def scan(expr):
   global token
-  if len(src) and src[0].isalpha(): token = variable();
-  elif len(src) and src[0].isdigit(): token = number()
-  elif len(src) and src[0] in '+-*/()': token = operator()
+  if len(expr) and expr[0].isdigit(): token = number(expr)
+  elif len(expr) and expr[0].isalpha(): token = variable(expr);
+  elif len(expr) and expr[0] in '+-*/()': token = operator(expr)
 
-# main
-print('EXPR:', ''.join(src))
-#print('eval()', eval(''.join(src)))
+def calc(expr):
+  #try: 
+    scan(e); result = expression(expr)
+    if result is not None: return result
+    else: print("Bad expressionession")
+  #except: print("Execution failed!")
 
-
-try: 
-  scan(); result = expr()
-  if result is not None: print('expr()', result)
-  else: print("Bad expression")
-except: print("Execution failed!")
-
+e = [i for i in '-(-((((7 + 2) * 5) / (numOne - 5)) + ((numTwo * 4) - (9 / 3))) - ((((8 + 2) - 1) * (4 + numTwo)) / (9 - 3)))'.replace(' ', '')]
+print(calc(e))

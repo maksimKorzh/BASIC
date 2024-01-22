60b7836c3a45423499a35d763f51318fd3817ee1
Jay Bhatt <jay.bhatt98@gmail.com>
2024-01-22 12:49:45 -0500

    Added helper methods to CommandInterpreter.py

--- src/CommandInterpreter.py
+++ src/CommandInterpreter.py
@@ -40,17 +40,32 @@ class CommandInterpreter:
         :param user_input: The user input to interpret.
         :return: None
         """
-        if user_input in self.commands:
-            try:
-                self.commands[user_input]()
-            except Exception as e:
-                print(f'The command failed for the following reason: "{e}".')
-        else:
-            try:
-                line_number, line_code = map(
-                    str.strip, user_input.split(" ", 1)
-                )
-                self.basic_interpreter.buffer[int(line_number)] = line_code
-            except Exception:
-                self.basic_interpreter.execute(user_input)
-                raise Exception
+        try:
+            if user_input in self.commands:
+                self.execute_command(user_input)
+            else:
+                self.process_basic_code(user_input)
+        except Exception as e:
+            print(f'The command failed for the following reason: "{e}".')
+
+    def execute_command(self, command: str) -> None:
+        """Execute a predefined command.
+
+        :param command: The command to execute.
+        :return: None
+        """
+        command_function = self.commands.get(command)
+        if command_function:
+            command_function()
+
+    def process_basic_code(self, user_input: str) -> None:
+        """Treat user input as BASIC code and store it in the buffer.
+
+        :param user_input: The user input to store in the buffer.
+        :return: None
+        """
+        try:
+            line_number, line_code = map(str.strip, user_input.split(" ", 1))
+            self.basic_interpreter.buffer[int(line_number)] = line_code
+        except Exception as e:
+            print(f'Error processing BASIC code: "{e}".')

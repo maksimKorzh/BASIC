0fd0915b3121575fa06159b5c8702298c740300e
Jay Bhatt <jay.bhatt98@gmail.com>
2024-01-22 12:17:11 -0500

    Tweaked CommandInterpreter.py

--- src/CommandInterpreter.py
+++ src/CommandInterpreter.py
@@ -1,28 +1,44 @@
-class CommandInterpreter:
-    """"""
-    def __init__(self, basic_interpreter, command_processor):
-        """
+from BasicInterpreter import BasicInterpreter
+from CommandProcessor import CommandProcessor
 
-        :param basic_interpreter:
-        :param command_processor:
+
+class CommandInterpreter:
+    """Interprets user commands for the BasicInterpreter."""
+
+    def __init__(
+        self,
+        basic_interpreter: BasicInterpreter,
+        command_processor: CommandProcessor,
+    ):
+        """Initialize the CommandInterpreter.
+
+        :param basic_interpreter: An instance of BasicInterpreter.
+        :param command_processor: An instance of CommandProcessor.
         """
         self.basic_interpreter = basic_interpreter
         self.command_processor = command_processor
         self.commands = {
             "clear": self.command_processor.clear_screen,
-            "list": lambda: self.command_processor.list_buffer(self.basic_interpreter.buffer),
-            "save": lambda: self.command_processor.save_file(self.basic_interpreter.buffer),
-            "load": lambda: self.command_processor.load_file(self.basic_interpreter.buffer),
-            "new": lambda: self.basic_interpreter.buffer.clear(),
+            "list": lambda: self.command_processor.list_buffer(
+                self.basic_interpreter.buffer
+            ),
+            "save": lambda: self.command_processor.save_file(
+                self.basic_interpreter.buffer
+            ),
+            "load": lambda: self.command_processor.load_file(
+                self.basic_interpreter.buffer
+            ),
+            "new": self.basic_interpreter.buffer.clear,
             "run": self.basic_interpreter.run,
-            "quit": lambda: exit(),
+            "quit": exit,
         }
+        """"""
 
-    def interpret_command(self, user_input):
-        """
+    def interpret_command(self, user_input: str) -> None:
+        """Interpret a user command.
 
-        :param user_input:
-        :return:
+        :param user_input: The user input to interpret.
+        :return: None
         """
         if user_input in self.commands:
             try:
@@ -34,5 +50,5 @@ class CommandInterpreter:
                 line_number, line_code = map(str.strip, user_input.split(" ", 1))
                 self.basic_interpreter.buffer[int(line_number)] = line_code
             except Exception:
-                self.basic_interpreter.execute(0, user_input)
+                self.basic_interpreter.execute(user_input)
                 raise Exception

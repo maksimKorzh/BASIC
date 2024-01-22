f1da58841d498921f16600bf7ba341d4f14c774d
Jay Bhatt <jay.bhatt98@gmail.com>
2024-01-22 11:57:57 -0500

    Fixed the import errors in Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,24 @@
-import BasicInterpreter
-import CommandProcessor
-import CommandInterpreter
+from BasicInterpreter import BasicInterpreter
+from CommandInterpreter import CommandInterpreter
+from CommandProcessor import CommandProcessor
 
 
-def main():
+def main() -> None:
+    """Represent the main function of the application.
+
+    :return: None.
+    """
     basic_interpreter = BasicInterpreter()
     command_processor = CommandProcessor()
-    command_interpreter = CommandInterpreter(basic_interpreter, command_processor)
+    command_interpreter = CommandInterpreter(
+        basic_interpreter,
+        command_processor,
+    )
 
+    # Continuously takes in inputs from the user and interprets them
     while True:
         user_input = input("> ")
-        command_interpreter.process_command(user_input)
+        command_interpreter.interpret_command(user_input)
 
 
 if __name__ == "__main__":
